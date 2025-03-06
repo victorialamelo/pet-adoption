587f4bbf504d72839b027fa9c52aeab9f0ef761a
victorialamelo <victoria.lamelo@gmail.com>
2025-03-06 17:59:00 +0100

    created jsx pages and routes

--- adoption/client/src/App.jsx
+++ adoption/client/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
+import { SignupPage } from "./pages/SignupPage";
+import { PetListPage } from "./pages/PetListPage";
+import { PetDetailsPage } from "./pages/PetDetailsPage";
+import { PostPetPage } from "./pages/PostPetPage";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        hola its Ash doing a test
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/petlist" element={<PetListPage />} />
+      <Route path="/petdetails" element={<PetDetailsPage />} />
+      <Route path="/postpet" element={<PostPetPage />} />
+    </Routes>
+  );
 }
-
-export default App
---
+++ adoption/client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router";
+
+
+export function HomePage() {
+  
+
+  return (
+   
+  );
+}
