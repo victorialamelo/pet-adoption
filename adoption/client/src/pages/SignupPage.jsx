7a3f6090c04c18183418a9b3e8b40832b571e1a1
victorialamelo <victoria.lamelo@gmail.com>
2025-03-11 13:27:15 +0100

    updated signuppage

--- adoption/client/src/pages/SignupPage.jsx
+++ adoption/client/src/pages/SignupPage.jsx
@@ -1,207 +1,282 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
-import NavBar from '../components/NavBar'
+import { useNavigate } from "react-router-dom";
+import NavBar from "../components/NavBar";
+// import { AddNewUser, backendAuthLogin, backendCreateUser } from "../backend";
+// import { hasSession, saveSession, getCurrentSession } from "../session";
 
 export default function SignupPage() {
-  const [emailError, setEmailError] = useState(false);
-  const [emailErrorMessage, setEmailErrorMessage] = useState('');
-  const [passwordError, setPasswordError] = useState(false);
-  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
-  const [nameError, setNameError] = useState(false);
-  const [nameErrorMessage, setNameErrorMessage] = useState('');
-  const [role, setRole] = useState(""); // "adopting" or "posting"
-  const [shelterName, setShelterName] = useState("");
+  const [role, setRole] = useState("");
+  const navigate = useNavigate();
 
-  const handleRoleChange = (e) => {
-    setRole(e.target.value);
-  };
-
-
-  const handleSubmit = async (event) => {
+  const createUser = async (formData, event) => {
     event.preventDefault();
+    const newUser = {
+      user_name: formData.get("name"),
+      date_of_birth: formData.get("date"),
+      city: formData.get("city"),
+      zipcode: Number(formData.get("zipcode")), // transform to number due to original string value
+      phone: Number(formData.get("phone")),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      entity_name: formData.get("shelter"),
+      entity_website: formData.get("website"),
+      entity_registration_id: Number(formData.get("registrationid")),
+    };
 
-    if (!validateInputs(event)) {
-      return;
-    }
+    // TODO: create backendCreateUser function in backend.js
+    const addedUser = await backendCreateUser(newUser); // send the new object (with all the input from new user) to backend
+
+    // TODO: integrate authLogin function in backend.js
+    const { token } = await backendAuthLogin({ email, password });
+    saveSession(token);
 
     console.log("handleSubmit Sign Up");
-    // const data = new FormData(event.currentTarget);
-    // const userData = {
-    //   name: data.get('name'),
-    //   username: data.get('email'),
-    //   password: data.get('password'),
-    // };
 
-    // const credentials = {
-    //   username: data.get('email'),
-    //   password: data.get('password'),
-    // };
-
-    // try {
-    //   const response = await axios.post("/api/auth/register", userData, {
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   });
-
-    //   if (response.status === 200) {
-    //     localStorage.setItem("token", response.data.token);
-    //     auth.login(credentials);
-    //   } else {
-    //     alert("Error: " + response.data.message);
-    //   }
-    // } catch (error) {
-    //   console.error("Error during registration:", error);
-    // }
+    // redirect user to Dashboard page/user_id
+    navigate(`/userdashboard/${addedUser.user_id}`);
   };
 
-  const validateInputs = (event) => {
-    event.preventDefault();
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
-    const name = document.getElementById('name');
+  return (
+    <>
+      <NavBar />
+      <div className="container my-5">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card shadow p-4">
+              <h1 className="text-center mb-3">Sign Up</h1>
+              <form action={createUser}>
+                {/* Full Name */}
+                <div className="mb-3">
+                  <label htmlFor="name" className="form-label">
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="form-control"
+                    placeholder="Enter your name"
+                    onInput={(e) => {
+                      let value = e.target.value;
+                      if (value.length > 0) {
+                        e.target.value =
+                          value.charAt(0).toUpperCase() + value.slice(1);
+                      }
+                    }}
+                    required
+                  />
+                </div>
 
-    let isValid = true;
+                {/* Date of Birth */}
+                <div className="mb-3">
+                  <label
+                    htmlFor="date"
+                    className="form-label"
+                    style={{ display: "block", marginBottom: "10px" }}
+                  >
+                    Date of Birth
+                  </label>
+                  <input
+                    type="date"
+                    id="date"
+                    name="date"
+                    style={{
+                      paddingRight: "5px",
+                      paddingLeft: "5px",
+                      borderRadius: "5px",
+                      fontSize: "15px",
+                      backgroundColor: "#fff",
+                      color: "#000",
+                    }}
+                    required
+                  />
+                </div>
 
-    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
-      setEmailError(true);
-      setEmailErrorMessage('Please enter a valid email address.');
-      isValid = false;
-    } else {
-      setEmailError(false);
-      setEmailErrorMessage('');
-    }
+                {/* City */}
+                <div className="mb-3">
+                  <label htmlFor="city" className="form-label">
+                    City
+                  </label>
+                  <input
+                    type="text"
+                    id="city"
+                    name="city"
+                    className="form-control"
+                    placeholder="City of residence"
+                    onInput={(e) => {
+                      let value = e.target.value;
+                      if (value.length > 0) {
+                        e.target.value =
+                          value.charAt(0).toUpperCase() + value.slice(1);
+                      }
+                    }}
+                    required
+                  />
+                </div>
 
-    if (!password.value || password.value.length < 6) {
-      setPasswordError(true);
-      setPasswordErrorMessage('Password must be at least 6 characters long.');
-      isValid = false;
-    } else {
-      setPasswordError(false);
-      setPasswordErrorMessage('');
-    }
+                {/* Zipcode */}
+                <div className="mb-3">
+                  <label htmlFor="zipcode" className="form-label">
+                    Zipcode
+                  </label>
+                  <input
+                    type="text"
+                    id="zipcode"
+                    name="zipcode"
+                    className="form-control"
+                    placeholder="08020"
+                    maxLength="5"
+                    inputMode="numeric"
+                    required
+                  />
+                </div>
 
-    if (!name.value || name.value.length < 1) {
-      setNameError(true);
-      setNameErrorMessage('Name is required.');
-      isValid = false;
-    } else {
-      setNameError(false);
-      setNameErrorMessage('');
-    }
+                {/* Phone */}
+                <div className="mb-3">
+                  <label htmlFor="phone" className="form-label">
+                    Phone
+                  </label>
+                  <input
+                    type="text"
+                    id="phone"
+                    name="phone"
+                    className="form-control"
+                    placeholder="665555333"
+                    maxLength="13"
+                    inputMode="numeric"
+                    defaultValue="+34 "
+                    required
+                  />
+                </div>
 
-    return isValid;
-  };
+                {/* Email */}
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="form-control"
+                    placeholder="your@email.com"
+                    required
+                  />
+                </div>
 
-    return (
-      <>
-        <NavBar />
-        <div className="container my-5">
-          <div className="row justify-content-center">
-            <div className="col-md-6">
-              <div className="signup card p-4">
-                <h1 className="text-center mb-3">Sign Up</h1>
-                <form onSubmit={handleSubmit}>
-                  {/* Full Name */}
-                  <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Full Name</label>
-                    <input type="text" id="name" name="name" className="form-control" placeholder="Enter your name" />
-                    {nameError && <p className="error-message">{nameErrorMessage}</p>}
+                {/* Password */}
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="form-control"
+                    placeholder="••••••••"
+                    required
+                  />
+                </div>
+
+                {/* Are they adopting or posting? */}
+                <div className="mb-3">
+                  <label className="form-label">
+                    Are you adopting or posting a pet?
+                  </label>
+                  <div className="form-check">
+                    <input
+                      type="radio"
+                      id="adopting"
+                      name="role"
+                      value="adopting"
+                      className="form-check-input"
+                      onChange={(e) => setRole(e.target.value)}
+                    />
+                    <label htmlFor="adopting" className="form-check-label">
+                      Adopting
+                    </label>
                   </div>
-
-                  {/* Email */}
-                  <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" id="email" name="email" className="form-control" placeholder="your@email.com" />
-                    {emailError && <p className="error-message">{emailErrorMessage}</p>}
+                  <div className="form-check">
+                    <input
+                      type="radio"
+                      id="posting"
+                      name="role"
+                      value="posting"
+                      className="form-check-input"
+                      onChange={(e) => setRole(e.target.value)}
+                    />
+                    <label htmlFor="posting" className="form-check-label">
+                      Posting a pet
+                    </label>
                   </div>
+                </div>
 
-                  {/* Password */}
+                {/* Shelter/Organization Name (Only if posting a pet) */}
+                {role === "posting" && (
                   <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" id="password" name="password" className="form-control" placeholder="••••••••" />
-                    {passwordError && <p className="error-message">{passwordErrorMessage}</p>}
-                  </div>
-
-                  {/* Are they adopting or posting? */}
-                  <div className="mb-3">
-                    <label className="form-label">Are you adopting or posting a pet?</label>
-                    <div className="form-check">
-                      <input
-                        type="radio"
-                        id="adopting"
-                        name="role"
-                        value="adopting"
-                        className="form-check-input"
-                        onChange={handleRoleChange}
-                      />
-                      <label htmlFor="adopting" className="form-check-label">Adopting</label>
-                    </div>
-                    <div className="form-check">
-                      <input
-                        type="radio"
-                        id="posting"
-                        name="role"
-                        value="posting"
-                        className="form-check-input"
-                        onChange={handleRoleChange}
-                      />
-                      <label htmlFor="posting" className="form-check-label">Posting a pet</label>
-                    </div>
-                  </div>
-
-                  {/* Shelter/Organization Name (Only if posting a pet) */}
-                  {role === "posting" && (
-                    <>
-                    <div className="mb-3">
-                      <label htmlFor="shelter" className="form-label">Organization Name (Optional)</label>
-                      <input
-                        type="text"
-                        id="shelter"
-                        name="shelter"
-                        className="form-control"
-                        placeholder="Enter organization name"
-                        value={shelterName}
-                        onChange={(e) => setShelterName(e.target.value)}
-                      />
-                    </div>
-                    <div className="mb-3">
-                    <label htmlFor="website" className="form-label">Organization Website (Optional)</label>
+                    <label htmlFor="shelter" className="form-label">
+                      Shelter/Organization Name (Optional)
+                    </label>
                     <input
                       type="text"
                       id="shelter"
                       name="shelter"
                       className="form-control"
-                      placeholder="Enter your organization's website"
-                      value={shelterName}
-                      onChange={(e) => setShelterName(e.target.value)}
+                      placeholder="Enter shelter name (if applicable)"
                     />
-                    </div>
-                    <div className="mb-3">
-                    <label htmlFor="registration-id" className="form-label">Organization Registration ID</label>
+                  </div>
+                )}
+
+                {role === "posting" && (
+                  <div className="mb-3">
+                    <label htmlFor="website" className="form-label">
+                      Shelter/Organization Website (Optional)
+                    </label>
                     <input
                       type="text"
-                      id="shelter"
-                      name="shelter"
+                      id="website"
+                      name="website"
                       className="form-control"
-                      placeholder="Enter your organization's registration ID/license"
-                      value={shelterName}
-                      onChange={(e) => setShelterName(e.target.value)}
+                      placeholder="www.website.com"
                     />
-                    </div>
-                  </>
-                  )}
+                  </div>
+                )}
 
-                  {/* Submit Button */}
-                  <button type="submit" className="btn btn-primary w-100">Sign Up</button>
-                </form>
+                {role === "posting" && (
+                  <div className="mb-3">
+                    <label htmlFor="registrationid" className="form-label">
+                      Shelter/Organization Registration Number (Optional)
+                    </label>
+                    <input
+                      type="text"
+                      id="registrationid"
+                      name="registrationid"
+                      maxLength="9"
+                      className="form-control"
+                      placeholder="A12345678"
+                    />
+                  </div>
+                )}
 
-                <hr />
-                <p className="text-center">Already have an account? <a href="/Login" className="text-decoration-none">Log in</a></p>
-              </div>
+                {/* Submit Button */}
+                <button type="submit" className="btn btn-primary w-100">
+                  Sign Up
+                </button>
+              </form>
+
+              <hr />
+              <p className="text-center">
+                Already have an account?{" "}
+                <a href="/Login" className="text-decoration-none">
+                  Log in
+                </a>
+              </p>
             </div>
           </div>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+}
