0945e0651690a253a8dc3ff7995fa78481e629be
ashlei-t <a.temena@gmail.com>
2025-03-14 16:09:42 +0100

    pet details populating

--- adoption/client/src/pages/PetDetailsPage.jsx
+++ adoption/client/src/pages/PetDetailsPage.jsx
@@ -12,8 +12,24 @@ export default function PetDetailsPage() {
   useEffect(() => {
     const fetchPetDetails = async () => {
       try {
-        const data = await backendFetchPetDetails(pet_id);
-        setPetDetails(data);
+        const fetchedPetData = await backendFetchPetDetails(pet_id);
+        console.log("FETCH PET DETAILS data", fetchedPetData[0]);
+        const petData = fetchedPetData[0];
+
+        const processedPetDetails = {
+          ...petData,
+          neutered: Boolean(petData.neutered),
+          has_special_needs: Boolean(petData.has_special_needs),
+          potty_trained: Boolean(petData.potty_trained),
+          goodWith: [
+            petData.good_with_cats ? "Cats" : null,
+            petData.good_with_dogs ? "Dogs" : null,
+            petData.good_with_kids ? "Kids" : null,
+            petData.good_with_smallspaces ? "Small Spaces" : null,
+          ].filter(Boolean),
+        };
+        console.log("processedPetDetails", processedPetDetails)
+        setPetDetails(processedPetDetails);
       } catch (err) {
         setError(err.message);
       } finally {
@@ -31,27 +47,31 @@ export default function PetDetailsPage() {
   return (
     <>
       <NavBar />
-      <section className="my-5">
+      <header>
+        <img src="../src/assets/beigekitten.jpg" alt="Pet Banner" />
+      </header>
+      <section className="mb-10">
         <div className="container">
           <div className="row">
             <div className="col-md-6">
               <h2 className="display-4">{petDetails.name}</h2>
-              <p className="lead">{petDetails.description}</p>
+              <p className="lead">{petDetails.pet_description}</p>
               <ul className="list-unstyled">
                 <li><strong>Size:</strong> {petDetails.size}</li>
-                <li><strong>Age:</strong> {petDetails.age} years</li>
                 <li><strong>Weight:</strong> {petDetails.weight} kg</li>
                 <li><strong>Gender:</strong> {petDetails.gender}</li>
-                <li><strong>Activity Level:</strong> {petDetails.activity}</li>
+                <li><strong>Activity Level:</strong> {petDetails.activity_level}</li>
                 <li><strong>Neutered:</strong> {petDetails.neutered ? "Yes" : "No"}</li>
                 <li><strong>Special Needs:</strong> {petDetails.specialNeeds ? "Yes" : "No"}</li>
                 <li><strong>Potty Trained:</strong> {petDetails.pottyTrained ? "Yes" : "No"}</li>
-                <li><strong>Good With:</strong> {petDetails.goodWith?.join(", ")}</li>
+                {petDetails.goodWith.length > 0 && (
+                <li><strong>Good With:</strong> {petDetails.goodWith.join(", ")}</li>
+              )}
               </ul>
             </div>
             <div className="col-md-6">
               <img
-                src={petDetails.image_url || "default-image.jpg"}
+                src={petDetails.image_url || "../src/assets/bluedog.jpg"}
                 alt={petDetails.name}
                 className="img-fluid rounded"
               />
